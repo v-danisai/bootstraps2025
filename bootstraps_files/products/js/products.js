8263c6c0a0111643f7f0a7ce1816e2b09a797d61
v-danisai <dv1190991@gmail.com>
2025-04-01 09:29:42 -0600

    Edit and Del
    functional, unfinished

--- bootstraps_files/products/js/products.js
+++ bootstraps_files/products/js/products.js
@@ -1,3 +1,4 @@
+var itemCounter = 0;
 $(document).ready(function(){
     
     // // On click listener
@@ -13,16 +14,16 @@ $(document).ready(function(){
 
     // On click listener for a class
         $(document).on("click", ".deleteButton", function(){
-           delItem(); 
+           delItem(this); 
         });
 
         $(document).on("click", ".editButton", function(){
             editItem(this); 
          });
 
-        $(document).on("click", ".editButton", function(){
-            editItem();
-        });
+        // $(document).on("click", ".editButton", function(){
+        //     editItem();
+        // });
 
     // On submit listener
         $("#newProductForm").submit(function(e){
@@ -34,51 +35,57 @@ $(document).ready(function(){
 // Function to edit item
 function editItem(btn){
     let name = $(btn).attr("dataName");
-    let desc = $(btn).attr("datDesc");
+    let desc = $(btn).attr("dataDesc");
     let price = $(btn).attr("dataPrice");
     let url = $(btn).attr("dataImage");
 
+    $("#editProduct").val(name);
+    $("#editDesc").val(desc);
+    $("#editPrice").val(price);
+    $("#editImage").val(url);
+    
     $("#editProductModal").modal("show");
 }
 
 // Function to delete item
-function delItem(){
-    $.confirm({
-        title: 'Encountered an error!',
-        content: 'Something went downhill, this may be serious',
-        type: 'red',
-        typeAnimated: true,
-        buttons: {
-            tryAgain: {
-                text: 'Try again',
-                btnClass: 'btn-red',
-                action: function(){
-                }
-            },
-            close: function () {
-            }
-        }
-    });
+function delItem(btn){
+    // $.confirm({
+    //     title: 'Encountered an error!',
+    //     content: 'Something went downhill, this may be serious',
+    //     type: 'red',
+    //     typeAnimated: true,
+    //     buttons: {
+    //         tryAgain: {
+    //             text: 'Try again',
+    //             btnClass: 'btn-red',
+    //             action: function(){
+    //             }
+    //         },
+    //         close: function () {
+    //         }
+    //     }
+    // });
+    let id = $(btn).attr("data-id");
+    $("#tr_"+id).fadeOut("slow");
 }
-
 
 //Function to register item
 function registerItem(){
+    itemCounter++;
+
     let itemName = $("#inpProduct").val();
     let itemDesc = $("#inpDesc").val();
     let itemPrice = $("#inpPrice").val();
     let itemImage = $("#inpImage").val();
 
-    // counter = counter + 1;
-
     let html = "";
 
-    html += "<tr>";
-    html += "<td>"+counter+"</td>";
+    html += "<tr id='tr_"+itemCounter+"'>";
+    html += "<td>"+itemCounter+"</td>";
     html += "<td> <img src='"+itemImage+"' width='75px' /> </td>";
     html += "<td><strong>"+itemName+"</strong><br><small>"+itemDesc+"</small></td>";
     html += "<td>"+itemPrice+"</td>";
-    html += "<td><button class='btn btn-outline-primary editButton'>Edit</button> <button class='btn btn-outline-danger deleteButton'>Delete</button></td>";
+    html += "<td><button  class='btn btn-outline-primary editButton' dataName="+itemName+" dataDesc="+itemDesc+" dataPrice="+itemPrice+" dataImage="+itemImage+">Edit</button> <button data-id="+itemCounter+" class='btn btn-outline-danger deleteButton'>Delete</button></td>";
     html +="</tr>";
 
     $("#tableProducts").append(html);
